a710f2b6e19f70206cf47c168e350c94a6cff508
davoud77 <davoud.fathi1377@gmail.com>
2023-02-02 18:18:19 +0330

    create fun main and name and family

--- app/src/main/java/com/example/git05/MainActivity.kt
+++ app/src/main/java/com/example/git05/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.git05
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        main()
+    }
+
+    private fun main() {
+        val name = "davoud"
+        val family = "fathi"
+
+        Toast.makeText(this, "$name and $family", Toast.LENGTH_SHORT).show()
     }
 }
